9b293e9427b4edcf7cb1b60affe34447b869843e
Omer Wazir <omer.wazir@gmail.com>
2014-03-11 17:06:02 -0700

    Passed all challenges. Yay!

---
+++ learnyounode/program13.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var util = require('util');
+var url = require('url');
+var util = require('util');
+
+var parseTime = function(isoDate){
+	var date = new Date(isoDate);
+	return { "hour": date.getHours(),
+			 "minute": date.getMinutes(),
+			 "second": date.getSeconds() };
+}
+
+var unixTime = function(isoDate){
+	var date = new Date(isoDate);
+	return {"unixtime": date.getTime() };
+}
+
+var server = http.createServer();
+
+server.on('request', function(req,res){
+	var parsedUrl = url.parse(req.url, true);
+	if(req.method === 'GET'){
+		if(parsedUrl.pathname === '/api/parsetime'){
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write(JSON.stringify(parseTime(parsedUrl.query.iso)));
+			res.end();	
+		} else if (parsedUrl.pathname === '/api/unixtime'){
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write(JSON.stringify(unixTime(parsedUrl.query.iso)));
+			res.end();	
+		} else {
+	 		res.writeHead(405, "Path not supported", {'Content-Type': 'text/html'});
+			res.end();				
+		}
+	} else {
+ 		res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
+		res.end();	
+	}
+});
+
+server.listen(Number(process.argv[2]));
+console.log('HTTP server listening on port: ' + process.argv[2]);
