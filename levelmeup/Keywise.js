b67f7e626e88bf99ed436569d2a5e5288c3ce543
Omer Wazir <omer.wazir@gmail.com>
2014-03-17 17:03:17 -0700

    Passed Keywise with much confusion

---
+++ levelmeup/Keywise.js
@@ -0,0 +1,14 @@
+var level = require('level')
+var db = level(process.argv[2], { valueEncoding: 'json' })
+var data = require(process.argv[3])
+
+var batchOperations = data.map(function (entry) {
+  var key;
+  if (entry.type === 'user')
+    key = entry.name;
+  else if (entry.type === 'repo')
+    key = entry.user + '!' + entry.name;
+  return { type: 'put', key: key, value: entry }
+})
+
+db.batch(batchOperations);
