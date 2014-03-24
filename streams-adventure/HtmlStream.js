158b6d7c3b00341ecc5f0a34c8fa130e2b91ee96
Omer Wazir <omer.wazir@gmail.com>
2014-03-24 11:19:03 -0700

    Passed HTML STREAM

---
+++ streams-adventure/HtmlStream.js
@@ -0,0 +1,18 @@
+var trumpet = require('trumpet');
+var through = require('through');
+var tr = trumpet();
+
+var caseConverter = through(function write(line){
+		this.queue(line.toString().toUpperCase());
+	},
+	function end(){
+		this.queue(null)
+});
+
+// tr.selectAll('.loud', function(text){
+// 	text.createStream().pipe(caseConverter).pipe(process.stdout);
+// });
+
+tr.select('.loud').createStream().pipe(caseConverter).pipe(process.stdout);
+tr.pipe(process.stdout);
+process.stdin.pipe(tr);
