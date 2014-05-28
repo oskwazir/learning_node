8139c2b78b32452b62b4a0200053007d195010e0
Omer Wazir <omer.wazir@gmail.com>
2014-05-28 15:33:15 -0700

    Passed duck typing

---
+++ functional/quacking.js
@@ -0,0 +1,8 @@
+
+function quackers() {
+  return Array.prototype.slice.call(arguments).filter(function(obj) {
+    return Object.prototype.hasOwnProperty.call(obj, 'quack')
+  }).length
+}
+
+module.exports = quackers
