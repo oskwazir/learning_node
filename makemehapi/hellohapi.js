6958826147487b2dd8fe63ed1918852ce65c3780
Omer Wazir <omer.wazir@gmail.com>
2015-02-09 16:44:23 -0700

    Updated to HAPI 8.1

--- makemehapi/hellohapi.js
+++ makemehapi/hellohapi.js
@@ -1,14 +1,19 @@
-var Hapi = require('hapi');
-var port = Number(process.argv[2] || 8080);
+"use strict";
 
-var server = Hapi.createServer('localhost',port)
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  host:'localhost',
+  port: Number(process.argv[2] || 8080)
+});
 
 server.route({
-    path: '/',
-    method: 'GET',
-    handler: function(request,reply){
-        reply('Hello Hapi');
-    }
-})
+  path:'/',
+  method:'GET',
+  handler: function handler(request,reply){
+    reply("Hello Hapi");
+  }
+});
 
 server.start();
